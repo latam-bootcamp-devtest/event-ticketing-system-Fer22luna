3ffe2bff3c96cafda18dd9b6d1aba2baa45de571
Fernando Gabriel Luna <fer22luna@gmail.com>
2025-03-08 10:31:43 -0300

    feat add Event entity

---
+++ EventTicketSystem.Server/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+using EventTicketSystem.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EventTicketSystem.Server.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+
+
+    }
+}
