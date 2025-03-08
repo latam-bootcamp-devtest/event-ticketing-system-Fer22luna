181281312839e707329bd9190fd5ce20d449b4c7
Fernando Gabriel Luna <fer22luna@gmail.com>
2025-03-08 11:18:45 -0300

    feat Adding Migrations for the table Event

---
+++ EventTicketSystem.Server/Migrations/20250308141611_AddTableEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventTicketSystem.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    eventId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    availableSeats = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.eventId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
--- EventTicketSystem.Server/obj/Debug/net8.0/EventTicketSystem.Server.AssemblyInfo.cs
+++ EventTicketSystem.Server/obj/Debug/net8.0/EventTicketSystem.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventTicketSystem.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7c00dbb26aad7198aca9c13d6b87bea28834a21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c4f9de4428a2ffa096743c365f6cb5aeee404b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventTicketSystem.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventTicketSystem.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
